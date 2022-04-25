5b95cba6e194fc13dfc3a95a83603b04beda6d29
zhao <physxzhao@gmail.com>
2022-04-26 00:35:25 +0800

    first commit

---
+++ ch1/1_1.cpp
@@ -0,0 +1,12 @@
+/*
+ * @Author: 赵世鑫
+ * @Date: 2022-04-26 00:33:21
+ * @LastEditors: 赵世鑫
+ * @LastEditTime: 2022-04-26 00:34:23
+ * @FilePath: /ch1/1_1.cpp
+ * @Description: 命令行 echo $? 得到结果 0
+ */
+
+int main(){
+    return 0;
+}
