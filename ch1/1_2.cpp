d3874c5477d68b34e67c2e4fcfe4baadbe9b5737
zhao <physxzhao@gmail.com>
2022-04-26 01:19:58 +0800

    1_2

---
+++ ch1/1_2.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: 赵世鑫
+ * @Date: 2022-04-26 00:57:04
+ * @LastEditors: 赵世鑫
+ * @LastEditTime: 2022-04-26 01:18:18
+ * @FilePath: /ch1/1_2.cpp
+ * @Description:
+ *      命令行输入 echo $? 得到结果为 255
+ *      因为unix系统退出状态码是无符号8位(0-255)
+ *      成功结束的退出码是0，错误的退出码可能是(1-255)
+ */
+
+int main(){
+    return -1;
+}
